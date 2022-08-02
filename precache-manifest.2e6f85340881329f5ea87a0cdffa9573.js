2aef17740c629711c452a50965709c214b3f62cb
jzambrano12 <jzambrano12@users.noreply.github.com>
2022-08-02 13:58:46 +0000

    deploy: 34df208de55b86535b048dd095f3b631a1984680

---
+++ precache-manifest.2e6f85340881329f5ea87a0cdffa9573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c010a21eedf0b2630cc26ac24b5ec200",
+    "url": "https://jzambrano12.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "27b2bd56d3715323b3aa",
+    "url": "https://jzambrano12.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "eafbdfee91964f80e760",
+    "url": "https://jzambrano12.github.io/task-management-frontend/static/js/2.9c9611f1.chunk.js"
+  },
+  {
+    "revision": "27b2bd56d3715323b3aa",
+    "url": "https://jzambrano12.github.io/task-management-frontend/static/js/main.b350005e.chunk.js"
+  },
+  {
+    "revision": "0ee08319758576885d1b",
+    "url": "https://jzambrano12.github.io/task-management-frontend/static/js/runtime~main.f8d58e1e.js"
+  }
+]);
